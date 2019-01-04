073fc3cb721c9ffe2d50da5e194efa53a155c6a8
gsmafra <gsenamafra@gmail.com>
2019-01-04 00:13:54 -0200

    circle 2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def run_setup():
 
     setup(
         name='sklearn_dummies',
-        version='0.2',
+        version='0.3',
         author='Gustavo Sena Mafra',
         author_email='gsenamafra@gmail.com',
         description='Scikit-learn label binarizer with support for missing values',
@@ -13,7 +13,7 @@ def run_setup():
         packages=find_packages(),
         url='https://github.com/gsmafra/sklearn-dummies',
         install_requires=['numpy', 'pandas', 'scikit-learn', 'pytest']
-        )
+    )
 
 
 if __name__ == '__main__':
